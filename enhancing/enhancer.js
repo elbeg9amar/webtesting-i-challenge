396a9a559a00056b3c75ac99552936d76e29431e
elbeg9amar <elbeg9amar@yahoo.com>
2020-10-15 02:16:12 -0400

    completed the project

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -7,27 +7,33 @@ module.exports = {
   repair,
   get,
 };
-
+let item = {
+  name:"Player",
+  enhancement : 15,
+  durability: 60 ,
+};
 function success(item) {
-  if(item.enchancement == 20) {
+  if(item.enhancement  == 20) {
       return item
   }else {
-    return  {...item, [item.enchancement]: item.enchancement + 1 }
+    return  {...item, [item.enhancement ]: item.enhancement  + 1 }
   }
 }
 
 function fail(item) {
+  
   if(item.enhancement < 15){
-    return {...item, [item.durability]: item.durability - 5 }
-  } else if (item.enhancement >= 15){
-    return {...item, [item.durability]: item.durability - 10 }
-  } else if (item.enhancement > 16){
-    return {...item, [item.enhancement]: item.enhancement - 1}
+    return {...item, durability: item.durability - 5 }
+  } else if (item.enhancement = 15){
+    return {...item, durability: item.durability - 10 }
+  } else {
+    return {...item, enhancement: item.enhancement - 1}
   }
 }
 
+
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,21 +3,46 @@ const enhancer = require('./enhancer.js');
 // test away!
 let item = {
     name:"Player",
-    enchancement: 20
-    ,
-    durability: 99 ,
+    enhancement : 15,
+    durability: 60 ,
 };
 
+describe('enhancer module', () => {
+
+    describe("enhancer success", () => {
+
+        it('if it succeed, it will add 1 on the enhancement ', () => {
+
+            expect(enhancer.success(item)).not.toBe(item);
+
+        });
+    });
+
+    describe('enhancer fail', () => {
+
+        it('if it fails, it will perform based on the condition', () => {
+
+            expect(enhancer.fail(item)).not.toBe(item);
+
+            expect(enhancer.fail({...item, enhancement: 5})).toStrictEqual({...item,enhancement: 5, durability: 55});
+
+            expect(enhancer.fail({...item, enhancement: 15})).toStrictEqual({...item,enhancement: 15, durability: 50});
+
+           
+        });
 
 
-it('if it succeed, it will add 1 on the enchancement', () => {
-    
-    expect(enhancer.success(item)).toStrictEqual(item);
-    // expect(enhancer.success({...item, [item.enchancement]
-    // : 20})).toBe({...item, [item.enchancement]
-    //     : 20})
+    });
+
+    describe('enhancer repair', () => {
+
+        it("it will repair the player and the durability restored to 100", () => {
+            expect(enhancer.repair(item)).toStrictEqual({...item, durability: 100})
+        }) ;
+    });
 });
 
-it('if it fails, it will perform based on the condition', () => {
-    expect(enhancer.fail(item)).not.toBe(item)
-})
+
+
+
+
